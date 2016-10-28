8bd282fcba9c5a4cc3aa16fd2f04937c0ee55a7b
wp <243550761@qq.com>
2016-10-28 14:20:08 +0800

    xxxxx

--- weibo.py
+++ weibo.py
@@ -5,7 +5,8 @@ import json
 import csv
 from bs4 import BeautifulSoup
 import codecs
-cookie = 'SINAGLOBAL=674332752823.8296.1444439311014; SSOLoginState=1477033633; wvr=6; _s_tentry=login.sina.com.cn; Apache=9710476368499.25.1477033646435; ULV=1477033646441:20:1:1:9710476368499.25.1477033646435:1471324921934; ULOGIN_IMG=14770336612731; login_sid_t=476b1d2fb9cd015acbb3a7a84b2cf327; UOR=developer.baidu.com,widget.weibo.com,login.sina.com.cn; SCF=AqliVXtf4Pe66qfvumAmHTUPB1o_gZQTktsys8qHcbBIQnaBzxJlV-NXobmpmgQZk7w3hluFfSGVgB2YlrmNWnY.; SUB=_2A251FsWoDeTxGedJ4lUW-CrEyziIHXVWZbBgrDV8PUNbmtBeLWXdkW8raK4yHGs_c2pI9vz-cd-5A9eLUw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5a44_asFynclfP5xSxg2HO5JpX5KMhUgL.Fo2N1KMN1hBRehB2dJLoIpXLxKMLBKnLB.zLxKnL12BL1h.LxKBLB.2LB.87M5tt; SUHB=0OMl_GV7uBH86Q; ALF=1509157233'
+cookie = 'xxxxx'
+#
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0',
     'cookie': cookie
